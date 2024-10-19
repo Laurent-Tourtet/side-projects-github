7e7b80e73bf65551c54405d5c68f51344a1085e4
Laurent-Tourtet <laurent.tourtet@oclock.school>
2024-10-19 17:03:23 +0200

    composant d'affichage d'image de profil installé et fonctionnel

--- galery/src/lib/components/GridGalery.svelte
+++ galery/src/lib/components/GridGalery.svelte
@@ -26,6 +26,7 @@
 </script>
 
 <style>
+   
     .carousel {
         position: relative;
         width: 100%;
@@ -34,15 +35,26 @@
         overflow: hidden;
     }
 
-    .carousel img {
-        width: 100%;
+    .carousel .slide {
         display: none;
+        text-align: center;
+        margin: 50px;
     }
 
-    .carousel img.active {
+    .carousel .slide.active {
         display: block;
     }
 
+    .carousel img {
+        width: 50%;
+        border: 1px solid black;
+        box-shadow:  2px 0 10px rgba(244, 243, 246, 0.678);
+    }
+.image-name {
+        margin-top: 10px;
+        font-size: 1.5em;
+        color: var(--link-color);
+}
     .carousel-buttons {
         position: absolute;
         top: 50%;
@@ -50,14 +62,21 @@
         display: flex;
         justify-content: space-between;
         transform: translateY(-50%);
+        
     }
 
     .carousel-buttons button {
-        background-color: rgba(0, 0, 0, 0.5);
+        width: 10%;
+        background-color: rgba(155, 150, 227, 0.5);
         color: white;
         border: none;
         padding: 10px;
         cursor: pointer;
+        border-radius: 10px;
+        
+    }
+    .carousel-buttons button:hover {
+        background-color: rgba(155, 150, 227, 0.8);
     }
 </style>
 
@@ -65,9 +84,13 @@
     <div class="carousel">
         {#if $images.length > 0}
             {#each $images as image, index}
-                <a href={image.profile_url} target="_blank">
-                    <img src={image.image_url} alt={image.name} class={index === currentIndex ? 'active' : ''}>
-                </a>
+                <div class="slide {index === currentIndex ? 'active' : ''}">
+                    <a href={image.profile_url} target="_blank">
+                        <img src={image.image_url} alt={image.name}>
+                    </a>
+                    <p class="image-name">{image.name}</p>
+                    <a class="image-profile" href="{image.profile_url}" target="_blank">Consulter le profil</a>
+                </div>
             {/each}
             <div class="carousel-buttons">
                 <button on:click={prevImage}>&lt;</button>
